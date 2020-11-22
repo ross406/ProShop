914a74adbe2f8f038d712f95e144ee344ffbf1ce
Roshan sharma <roshansharma406@gmail.com>
2020-11-22 13:44:35 +0530

    Navbar and Home Page started

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import Footer from './componenets/Footer';
+import Header from './componenets/Header';
 
 const App = () => {
   return (
-    <div className='App'>
-      <h1>ProShop</h1>
-    </div>
+    <>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>ProShop </h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
   );
 };
 
