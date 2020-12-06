f39e4970ab088fdb3367e1ebb54b9adb1d4999e5
Roshan sharma <roshansharma406@gmail.com>
2020-12-07 00:54:42 +0530

    Full PayPal Payment Functionality Added

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -1,6 +1,8 @@
+import { ORDER_LIST_MY_RESET } from '../constants/orderConstants';
 import {
   USER_DETAILS_FAIL,
   USER_DETAILS_REQUEST,
+  USER_DETAILS_RESET,
   USER_DETAILS_SUCCESS,
   USER_LOGIN_FAIL,
   USER_LOGIN_REQUEST,
@@ -51,7 +53,10 @@ export const userDetailsReducer = (state = { user: {} }, action) => {
       return { loading: false, user: action.payload };
     case USER_DETAILS_FAIL:
       return { loading: false, error: action.payload };
-
+    case USER_DETAILS_RESET:
+      return {
+        user: {},
+      };
     default:
       return state;
   }
