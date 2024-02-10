12a487936472e5cb3cc983b53c6b2aaa0d18fb2a
ross406 <roshansharma406@gmail.com>
2024-02-10 16:26:54 +0530

    removed backend folder and added backend files in root

--- backend/utils/constants.js
+++
@@ -1,3 +0,0 @@
-export const MONGO_URI = "mongodb://localhost:27017/proshop"
-export const JWT_SECRET = "secret"
-export const PAYPAL_CLIENT_ID= ""
---
+++ utils/constants.js
@@ -0,0 +1,4 @@
+// export const MONGO_URI = "mongodb://localhost:27017/proshop"
+export const MONGO_URI = "mongodb+srv://roshan:roshan@cluster0.yrus6.mongodb.net/proshop?retryWrites=true&w=majority"
+export const JWT_SECRET = "secret"
+export const PAYPAL_CLIENT_ID= ""
